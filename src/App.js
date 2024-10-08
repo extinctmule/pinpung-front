0e99d8f6da6704856b9fa719392487c68156fc03
extinctmule <extinctmule@gmail.com>
2024-10-09 00:59:39 +0900

    update: 초기 설정

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import "./styles/responsive.css";
+import Map from "./components/Map";
+
+function App() {
+  return (
+    <>
+      <Map />
+    </>
+  );
+}
+
+export default App;
